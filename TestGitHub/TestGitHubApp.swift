488959cd12694f3ae8bcb384b38bb08ee6421a99
Vladyslav <vladikbraun@gmail.com>
2022-01-25 15:25:39 -0800

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Vladyslav Brumar on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
